e3e8d2ed7f7b3b1a03b7917bfeac17eba608c436
vierre <r.vacca@studium.unict.it>
2017-10-25 16:02:30 +0200

    First commit

---
+++ corsoIngProject/corsoIngProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace corsoIngProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            Task restService = host.RunAsync();
+
+            restService.Wait();
+
+        }
+    }
+}
