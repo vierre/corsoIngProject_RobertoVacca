771f177786e01832e4ee6efabb7a5a3b647dd86b
vierre <r.vacca@studium.unict.it>
2017-10-27 11:08:57 +0200

    Added DAL classes Student & Exam

---
+++ corsoIngProject/corsoIngProject/Dal/Exam.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace corsoIngProject.Dal
+{
+    public class Exam
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Subject { get; set; }
+        public DateTime Date { get; set; }
+        public int Grade { get; set; }
+        public List<string> ArgumentList { get; set; }
+    }
+}
---
+++ corsoIngProject/corsoIngProject/Dal/Student.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace corsoIngProject.Dal
+{
+    public class Student
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ImmatricolationDate { get; set; }
+    }
+}
