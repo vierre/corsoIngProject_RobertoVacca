823d30cca2c4d72d7908e66b90e3c66eb5d2a56d
vierre <r.vacca@studium.unict.it>
2017-10-27 18:47:28 +0200

    fixed Exam & Student Classes

--- corsoIngProject/corsoIngProject/Dal/Exam.cs
+++ corsoIngProject/corsoIngProject/Dal/Exam.cs
@@ -1,15 +1,11 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace corsoIngProject.Dal
 {
     public class Exam
     {
-        [BsonId]
-        public ObjectId Id { get; set; }
+        public Guid Id { get; set; }
         public string Subject { get; set; }
         public DateTime Date { get; set; }
         public int Grade { get; set; }
--- corsoIngProject/corsoIngProject/Dal/Student.cs
+++ corsoIngProject/corsoIngProject/Dal/Student.cs
@@ -1,16 +1,13 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace corsoIngProject.Dal
 {
     public class Student
     {
-        [BsonId]
-        public ObjectId Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime ImmatricolationDate { get; set; }
+        public List<Exam> ExamsList { get; set; }
     }
 }
