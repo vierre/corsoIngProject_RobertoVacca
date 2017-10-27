7c30a9a12c1bee12c58adcc4c13b2e99a1934703
vierre <r.vacca@studium.unict.it>
2017-10-27 18:49:34 +0200

    added IStudentRepo interface

---
+++ corsoIngProject/corsoIngProject/IStudentsRepo.cs
@@ -0,0 +1,17 @@
+﻿using corsoIngProject.Dal;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace corsoIngProject
+{
+    interface IStudentsRepo
+    {
+        Task<IEnumerable<Student>> GetStudents();
+        Task<Student> GetStudent(object Id);
+        Task<CreatedResult> CreateStudent(Student student);
+        Task DeleteStudent(object Id);
+        Task UpdateStudent(Student student);
+        Task DeleteAllStudents(); 
+    }
+}
